ab8601bf022bbbbd02534a6220cea4d39d444e32
daironromano <daironromano@gmail.com>
2025-08-21 20:40:41 -0300

    Calculadora usando condição match case

--- Exercicios/calculadoraSimples.py
+++ Exercicios/calculadoraSimples.py
@@ -1,3 +1,22 @@
 num1 = int(input('Digite um valor: '))
 num2 = int(input('Digite um valor: ')) 
 op = str(input('Digite a operação: '))
+
+match op:
+    case '+':
+        r = num1 + num2
+        print(r)
+    case '-':
+        r = num1 - num2
+        print(r)
+    case '/':
+        if num2 == 0:
+            print('Não existe divisão por zero.')
+        else:
+            r = num1 / num2 
+            print(r)
+    case '*':
+        r = num1 * num2
+    case _:
+        print('Opção inválida.')
+    
