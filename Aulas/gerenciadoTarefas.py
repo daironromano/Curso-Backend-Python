4c034fc34bec4b2f98190a0309ff2d0c67547fd6
daironromano <daironromano@gmail.com>
2025-08-29 17:12:09 -0300

    Finalizando código de gerenciador de tarefas

--- Aulas/gerenciadoTarefas.py
+++ Aulas/gerenciadoTarefas.py
@@ -1,37 +1,50 @@
-
+#Função para adicionar uma nova tarefa
 def add_tarefa(tarefas):
     titulo = input('Digite o título da tarefa: ')
     tarefas.append({'titulo': titulo, 'concluida': False})
 
+#Função para listar as tarefas
 def listar_tarefas(tarefas):
     for i, tarefa in enumerate(tarefas, start=0):
-        print(f'{i}ª, {tarefa}')
-        
+        print(f'{i+1}ª, {tarefa}')
+
+#Função para marcar como concluída as tarefas    
 def marcar_concluida(tarefas):
     listar_tarefas(tarefas)
-
-    num = int(input('Qual tarefa marcar: '))
-    tarefas[num - 1]['concluida'] = True
-    print(f'Tarefa {num} concluída!')
+    try:
+        num = int(input('Qual tarefa marcar: '))
+        if 1 <= num <= len(tarefas):
+            tarefas[num - 1]['concluida'] = True
+            print(f'Tarefa {num} concluída!')
+        else:
+            print('Número de tarefa inválido!')
+    except ValueError:
+        print('Entrada inválida. Digite um número.')
 
 def sair():
     return False
 
 def main():
     tarefas = []
+    #dicionário fazendo referência as funções
     opcao = {
-        '1': add_tarefa(),
-        '2': listar_tarefas(),
-        '3': marcar_concluida(),
-        '4': sair()
+        '1': add_tarefa,
+        '2': listar_tarefas,
+        '3': marcar_concluida,
+        '4': sair
     }
 
     continuar = True
     while continuar:
         print(opcao)
         escolha = input('Qual a sua escolha: ')
-        funcoes = opcao.get(escolha)
-        if funcoes:
-            if funcoes(tarefas) == False:
-                continuar == False
+        funcao_escolhida = opcao.get(escolha)
+        if funcao_escolhida:
+            if escolha == '4':
+                continuar = funcao_escolhida()
+            else:
+                funcao_escolhida(tarefas)
+        else:
+            print('Opção inválida. Tente novamente.')
 
+main()
