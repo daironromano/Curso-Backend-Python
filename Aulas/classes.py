93da4bed27a4fe0d60b2ebbf8078b7e03a954dca
daironromano <daironromano@gmail.com>
2025-09-11 21:22:52 -0300

    Exercício de classe

--- Aulas/classes.py
+++ Aulas/classes.py
@@ -1,6 +1,6 @@
 class ContaBancaria:
     # Caracteríticas = Atributos
-    def __init__(self, numero, titular, saldo=0, limite=1000):
+    def __init__(self, numero, titular, saldo=0, limite=1200):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
@@ -11,10 +11,26 @@ class ContaBancaria:
     def depositar(self, valor):
         if valor > 0:
             self.saldo += valor
-        print('Seu saldo foi atualizado!')
+            self.transacoes.append(f"Depósito de R$ {valor:.2f} realizado!")
+            return "Seu saldo foi atualizado com sucesso!"
+        else:
+            return "Valor de depósito inválido!"
+
+    def sacar(self, valor):
+        if valor > 0 and valor <= (self.saldo + self.limite):
+            self.saldo -= valor
+            self.transacoes.append(f"Saque de R${valor:.2f } realizado!")
+            return "Seu saque foi realizado com sucesso!"
+        else:
+            return "Valor do saque indisponível!"
         
-    def sacar():
-        pass
-    def saldo():
-        pass
-contaDairon = ContaBancaria("001", "Dairon", "2000", "500")
+    def consultar_saldo(self):
+        return f"Valor atual: R$ {self.saldo}"
+
+
+contaDairon = ContaBancaria("001", "Dairon", 2000)
+contaFilite = ContaBancaria("002", "Filite", 1500, 2100)
+
+print(contaDairon.depositar(600))
+print(contaDairon.consultar_saldo())
+
