c578adc75fc70295ff71986962de178bd9d78920
daironromano <daironromano@gmail.com>
2025-09-18 20:20:45 -0300

    finalizando código de abstração

--- Aulas/classe_animal.py
+++ Aulas/classe_animal.py
@@ -6,24 +6,32 @@ class Animal:
         self.vivo = True
         self.fome = 50
 
-        def comer(self):
-            if self.vivo:
-                self.fome = 10
-                print(f'{self.nome} está comendo!')
-            else:
-                return f'O animal foi de arrasta!'
+    def comer(self):
+        if self.vivo:
+            self.fome += 10
+            print(f'{self.nome} está comendo!')
+        else:
+            return f'O animal foi de arrasta!'
 
-        def dormir(self):
-            if self.vivo:
-                return f'{self.nome} está dormindo'
-            else:
-                return f'{self.nome} não pode dormir'
+    def dormir(self):
+        if self.vivo:
+            self.fome -= 10
+            return f'{self.nome} está dormindo'
+        else:
+            return f'{self.nome} não pode dormir'
 
-        def mover(self):
-            pass
+    def mover(self):
+        if self.vivo:
+            return f'{self.nome} pode se mover'
+        else:
+            return f'{self.nome} foi de arrasta!@'
 
-        def emitir_som(self):
-            if self.vivo:
-                return f'{self.nome} emite um som'
-            else:
-                return f'{self.nome} não pode emitir som'
+    def emitir_som(self):
+        if self.vivo:
+            return f'{self.nome} emite um som'
+        else:
+            return f'{self.nome} não pode emitir som'
+            
+animal = Animal("João", 150, 52.6)
+print(animal.nome)
+animal.dormir()
