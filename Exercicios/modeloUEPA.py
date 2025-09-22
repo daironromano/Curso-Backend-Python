f3226d6b5bdcd2cccd5d7dbc795de7a7cd4342e1
daironromano <daironromano@gmail.com>
2025-09-22 17:40:28 -0300

    tarefa modelo-poo conluida

--- Exercicios/modeloUEPA.py
+++ Exercicios/modeloUEPA.py
@@ -5,7 +5,7 @@ class MembroUEPA:
         self.email = email
 
     def apresentar(self):
-        print(f'Membro da UFPA')
+        print(f'Membro da UEPA')
 
 class Aluno(MembroUEPA):
     def __init__(self, nome, matricula, email, curso, nota):
@@ -13,14 +13,15 @@ class Aluno(MembroUEPA):
         self.curso = curso
         self.nota = nota
     
-    def verificar_notas(self, nome, matricula, nota):
+    def verificar_notas(self):
         if self.nota < 5:
-            print(f'Aluno: {self.nome}\nMatrícula: {self.matricula}\nStatus: Reprovado')
+            print(f'Discente: {self.nome}\nMatrícula: {self.matricula}\nStatus: Reprovado')
         else:
-            print(f'Aluno: {self.nome}\nMatrícula: {self.matricula}\nStatus: Aprovado')
+            print(f'Discente: {self.nome}\nMatrícula: {self.matricula}\nStatus: Aprovado')
 
-    def apresentar(self, nome, curso):
-        print(f'{super().apresentar()}\nNome: {self.nome}\nCurso: {self.curso}')
+    def apresentar(self):
+        super().apresentar()
+        print(f'Discente: {self.nome}\nCurso: {self.curso}')
 
 class Professor(MembroUEPA):
     def __init__(self, nome, matricula, email, departamento):
@@ -28,17 +29,29 @@ class Professor(MembroUEPA):
         self.departamento = departamento
         self.frequencia = 0
 
-    def lancar_frequencia(self, frequencia):
-        pass
+    def lancar_frequencia(self, presente):
+        if presente.lower() == 's':
+            self.frequencia += 1
+            print(f'Discente {self.nome} presente!\nFrequência: {self.frequencia}')
+        else:
+            print(f'Discente {self.nome} faltou!\nFrequência: {self.frequencia}')
 
     def apresentar(self):
-        pass
+        super().apresentar()
+        print(f'Professor: {self.nome}\nDepartamento: {self.departamento}')
 
 # Testando classe membro
 Membro1 = MembroUEPA('dairon', 44005545, 'dr@gmail.com')
 Membro1.apresentar()
-
+print('\n')
 # Testando classe aluno 
 Aluno1 = Aluno('dairon', 44005545, 'dr@gmail.com', 'eng.comp', 7)
-Aluno1.verificar_notas('dairon', '44005545', 7)
-Aluno1.apresentar('dairon', 'eng.comp')
+Aluno1.verificar_notas()
+print('\n')
+Aluno1.apresentar()
+print('\n')
+# Testando classe professor
+Professor1 = Professor('dairon', 44005545, 'dr@gmail.com', 'Engenharia')
+Professor1.lancar_frequencia('n')
+print('\n')
+Professor1.apresentar()
