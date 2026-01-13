373900484344bc58eb27646ccaa3408f1594251b
daironromano <daironromano@gmail.com>
2026-01-12 22:16:21 -0300

    feat: templates adicionados

--- Aulas/Django/cinetech/cine/urls.py
+++ Aulas/Django/cinetech/cine/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 
-urlpatters = [
+urlpatterns = [
     path('', views.index, name='index'),
     path('add/<int:filme_id>/', views.adicionar_ao_pedido, name='add_pedido'),
     path('sacola/', views.ver_sacola, name='sacola'),
