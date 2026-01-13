8e9d29d79091506819e5534a1ece3126b04d18f5
daironromano <daironromano@gmail.com>
2026-01-12 21:20:28 -0300

    feat: funções adicionadas na view

--- Aulas/Django/cinetech/cine/views.py
+++ Aulas/Django/cinetech/cine/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 from .models import Filmes
-from .forms import ExpectadorForm
+from .forms import EspectadorForm
 
 # Create your views here.
 
@@ -23,6 +23,39 @@ def limpar_sacola(request):
     
 # Rascunho & Checkout 
 def ver_sacola(request):
+    sacola = request.session.get('ingressos_selecionados', {})
+    filmes_obj = []
+    total = 0
+
+    for f_id, qtd in sacola.items():
+        filme = get_object_or_404(Filme, pk=f_id)
+        subtotal = filme.preco * qtd
+        total += subtotal
+        filmes_obj.append({ 'filme': filme,
+                            'quantidade': qtd,
+                            'subtotal': subtotal})
     
+    if  request.method == 'POST':
+        request.session['rascunho_dados'] = request.POST
+        request.session.modified = True
+        form = EspectadorForm(request.POST)
+        messages.info(request, 'Rascunho salvo!')
+    else:
+        dados_salvos = request.session.get('rascunho_dados')
+        form = EspectadorForm(dados_salvos)
+    
+    return render(request,
+                  'sacola.html',
+                  {'itens': filmes_obj,
+                   'total': total,
+                   'form': form})
 
 # Controle de Acesso
+def index(request):
+    filmes = Filmes.objects.all()
+    qtd = len(request.session.get('ingressos_selecionados', {}))
+    return render(request,
+                  'index.html'
+                  {'filme': filmes,
+                    'qtd': qtd})
+
